6facc312faa857977dbfb5f65ad07ba07b8c3657
Ariel Ivan Diaz <arielivandiaz@gmail.com>
2020-09-14 20:12:44 -0300

    clean

---
+++ lagunite.js
@@ -0,0 +1,17 @@
+    function show(id) {
+        document.getElementById(id).classList.remove("hidden");
+    }
+
+    function hidde(id) {
+        document.getElementById(id).classList.add("hidden");
+    }
+
+    function toggle(id) {
+        document.getElementById(id).classList.toggle("hidden");
+    }
+
+
+    function toggleItems(id1, id2) {
+        toggle(id1);
+        toggle(id2)
+    }
